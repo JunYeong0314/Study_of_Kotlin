62101466c3d4bb8d63001467a7d414855fa4ed86
JunYeong0314 <kkmtk00314@naver.com>
2023-06-26 19:32:12 +0900

    baekjoon 1018

--- Baekjoon_prepare/src/main/kotlin/Main.kt
+++ Baekjoon_prepare/src/main/kotlin/Main.kt
@@ -1,22 +1,59 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.StringBuilder
+import java.util.Arrays
 import java.util.Collections
 import java.util.LinkedList
 
-fun main(args: Array<String>) {
-    var n = readLine()?.toInt() ?: return
-    n -= 1
-    var first = 666
+lateinit var matrix : Array<Array<Boolean>>
+var min = 64
 
-    while(n != 0){
-        first += 1
-        if(first.toString().contains("666")){
-            n -= 1
+fun main(args: Array<String>) {
+    val (row, col) = readLine()?.split(" ")?.map { it.toInt() } ?: return
+    matrix = Array(row) {Array(col) {true} }
+
+    for(i in 0 until row){
+        val input = readLine() ?: return
+        if(input.length != col){
+            println("Invalid input")
+            return
+        }
+        for(j in 0 until col){
+            if(input[j].toString() == "W"){
+                matrix[i][j] = true
+            }else{
+                matrix[i][j] = false
+            }
         }
     }
+    val N_row = row - 7
+    val N_col = col - 7
 
-    print("$first")
+    for(i in 0 until N_row){
+        for(j in 0 until N_col){
+            find(i, j)
+        }
+    }
+    println("$min")
+}
 
+fun find(x : Int, y : Int){
+    val end_x = x+8
+    val end_y = y+8
+    var cnt = 0
 
+    var TF = matrix[x][y]
+
+    for(i in x until end_x){
+        for(j in y until end_y){
+            if(matrix[i][j] != TF){
+                cnt++
+            }
+            TF = (!TF)
+        }
+        TF = !TF
+    }
+
+    cnt = Math.min(cnt, 64-cnt)
+    min = Math.min(min, cnt)
 }
