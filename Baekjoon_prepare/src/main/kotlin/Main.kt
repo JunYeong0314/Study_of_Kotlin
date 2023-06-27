5567a0015f22057e73d648cb40a2586b6813a7d3
JunYeong0314 <kkmtk00314@naver.com>
2023-06-27 19:29:45 +0900

    baekjoon 18870

--- Baekjoon_prepare/src/main/kotlin/Main.kt
+++ Baekjoon_prepare/src/main/kotlin/Main.kt
@@ -1,59 +1,21 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.StringBuilder
-import java.util.Arrays
-import java.util.Collections
-import java.util.LinkedList
-
-lateinit var matrix : Array<Array<Boolean>>
-var min = 64
 
 fun main(args: Array<String>) {
-    val (row, col) = readLine()?.split(" ")?.map { it.toInt() } ?: return
-    matrix = Array(row) {Array(col) {true} }
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine()?.toInt() ?: return
 
-    for(i in 0 until row){
-        val input = readLine() ?: return
-        if(input.length != col){
-            println("Invalid input")
-            return
-        }
-        for(j in 0 until col){
-            if(input[j].toString() == "W"){
-                matrix[i][j] = true
-            }else{
-                matrix[i][j] = false
-            }
-        }
-    }
-    val N_row = row - 7
-    val N_col = col - 7
+    val input = br.readLine() ?: return
+    val element = input.split(" ").map { it.toInt() }.toIntArray()
+    val sortedElement = element.sorted()
 
-    for(i in 0 until N_row){
-        for(j in 0 until N_col){
-            find(i, j)
-        }
-    }
-    println("$min")
-}
+    val rankMap = sortedElement.distinct().withIndex().associate { it.value to it.index }
 
-fun find(x : Int, y : Int){
-    val end_x = x+8
-    val end_y = y+8
-    var cnt = 0
-
-    var TF = matrix[x][y]
-
-    for(i in x until end_x){
-        for(j in y until end_y){
-            if(matrix[i][j] != TF){
-                cnt++
-            }
-            TF = (!TF)
-        }
-        TF = !TF
+    val output = StringBuilder()
+    for (i in element) {
+        output.append(rankMap[i]).append(" ")
     }
 
-    cnt = Math.min(cnt, 64-cnt)
-    min = Math.min(min, cnt)
+    println(output.toString())
 }
