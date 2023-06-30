ce7f3917f75767a45c07d8a17b50331e9ccb198f
JunYeong0314 <kkmtk00314@naver.com>
2023-06-30 17:23:37 +0900

    baekjoon 1012

--- Baekjoon_prepare/src/main/kotlin/Main.kt
+++ Baekjoon_prepare/src/main/kotlin/Main.kt
@@ -1,83 +1,62 @@
 import java.util.LinkedList
 import java.util.Queue
-import java.util.Scanner
-import java.util.StringTokenizer
-import kotlin.properties.Delegates
 
-lateinit var connected: Array<Array<Int>>
-lateinit var dfsVisited: Array<Boolean>
 lateinit var visited: Array<Array<Boolean>>
-lateinit var apart: Array<Int>
-val dx = arrayOf(0, 0, 1, -1)
-val dy = arrayOf(1, -1, 0, 0)
-var appartNum = 0
-var N = 0
+lateinit var farm : Array<Array<Int>>
+lateinit var numArr : Array<Int>
+val dCol = arrayOf(0, 0, 1, -1) // Same left and right
+val dRow = arrayOf(1, -1, 0, 0) // Same up and down
 
 
 fun main() {
-    N = readLine()?.toInt() ?: return
-    val scanner = Scanner(System.`in`)
-    connected = Array(N){ Array(N) { 0 } }
-    visited = Array(N){ Array(N) { false } }
-    apart = Array(N*N){ 0 }
+    val T = readLine()?.toInt() ?: return // Number of iterations
+    numArr = Array(T) { 0 } // Counter of bug number
 
-    for(i in 0 until N){
-        val input = readLine()
-        for(j in 0 until N){
-            connected[i][j] = input?.get(j)?.toString()?.toInt() ?: 0
+    for(i in 0 until T){
+        val (M, N, K) = readLine()?.split(" ")?.map { it.toInt() } ?: return // Input number of horizontal, veltical and cabbage
+        farm = Array(N){Array(M) { 0 }}
+        visited = Array(N){Array(M) { false } }
+        for(j in 0 until K){
+            val (col, row) = readLine()?.split(" ")?.map { it.toInt() } ?: return // Input of cabbage location
+            farm[row][col] = 1
         }
-    }
 
-    for(i in 0 until N){
-        for(j in 0 until N){
-            if(connected[i][j] == 1 && !visited[i][j]){
-                appartNum++
-                BFS(i, j)
-            }
-        }
-    }
-
-    apart.sort()
-    println("$appartNum")
-
-    apart.forEach { v->
-        if(v != 0) println(v)
-    }
-
-}
-
-fun BFS(x : Int, y : Int){
-    val q: Queue<IntArray> = LinkedList()
-    q.add(intArrayOf(x, y))
-    visited[x][y] = true
-    apart[appartNum]++
-
-    while(!q.isEmpty()){
-        val curX = q.peek()[0]
-        val curY = q.peek()[1]
-        q.poll()
-
-        for(i in 0 until 4){
-            val nx = curX + dx[i]
-            val ny = curY + dy[i]
-
-            if(nx >= 0 && ny >= 0 && nx < N && ny < N){
-                if(connected[nx][ny] == 1 && !visited[nx][ny]){
-                    q.add(intArrayOf(nx, ny))
-                    visited[nx][ny] = true
-                    apart[appartNum]++
+        for(k in 0 until N){
+            for(l in 0 until M){
+                if(farm[k][l] == 1 && !visited[k][l]){
+                    BFS(l, k, M, N, i)
                 }
             }
         }
     }
-}
-fun DFS(s : Int){
-    if(dfsVisited[s]) return
-    dfsVisited[s] = true
-    print("$s ")
 
-    connected[s].forEach { next->
-        DFS(next)
+    numArr.forEach { v->
+        println(v)
     }
 
+}
+
+fun BFS(col : Int, row : Int, M : Int, N : Int, index : Int){
+    visited[row][col] = true
+    val q : Queue<IntArray> = LinkedList()
+    q.add(intArrayOf(row, col))
+
+    while(!q.isEmpty()){
+        val curCol = q.peek()[1] // Current horizontal location
+        val curRow = q.peek()[0] // Current vertical location
+        q.poll()
+
+        for(i in 0 until 4){
+            val nCol = curCol + dCol[i] // Horizontal adjacency
+            val nRow = curRow + dRow[i] // Vertical adjacency
+
+            if(nCol >= 0 && nRow >= 0 && nCol < M && nRow < N){
+                if(farm[nRow][nCol] == 1 && !visited[nRow][nCol]){
+                    q.add(intArrayOf(nRow, nCol))
+                    visited[nRow][nCol] = true
+                }
+            }
+        }
+    }
+    numArr[index]++
 }
