e4f82f80f9d171c459fa429fd1119d591bf83fb5
JunYeong0314 <kkmtk00314@naver.com>
2023-07-07 16:52:17 +0900

    baekjoon 1697

--- Baekjoon_prepare/src/main/kotlin/Main.kt
+++ Baekjoon_prepare/src/main/kotlin/Main.kt
@@ -1,70 +1,41 @@
 import java.util.LinkedList
 import java.util.Queue
 
-data class Point(val row: Int, val col: Int)
-
-lateinit var visited: Array<Array<Boolean>>
-lateinit var farm : Array<Array<Int>>
-lateinit var maze : Array<Array<Int>>
-val dCol = arrayOf(0, 0, 1, -1) // Same left and right
-val dRow = arrayOf(1, -1, 0, 0) // Same up and down
-
-
+val visited = Array(100001){ 0 }
 fun main() {
-    val (row, col) = readLine()?.split(" ")?.map { it.toInt() } ?: return
-    maze = Array(row){Array(col) {0}}
-    visited = Array(row){Array(col) {false} }
+    var (x, k) = readLine()?.split(" ")?.map { it.toInt() } ?: return
 
-    for(i in 0 until row){
-        val input = readLine()
-        for(j in 0 until col){
-            maze[i][j] = input?.substring(j, j+1)?.toInt() ?: 0
-        }
-    }
-
-    val shortPathLength = BFS(Point(0, 0), Point(row-1, col-1))
-    if(shortPathLength != -1){
-        println(shortPathLength)
-    }else{
-        println("ERROR")
-    }
+    println(BFS(x, k))
 
 }
 
-fun BFS(start: Point, end: Point): Int{
-    val queue: Queue<Point> = LinkedList()
-    queue.offer(start)
-    visited[start.row][start.col] = true
+fun BFS(x: Int, k: Int): Int{
+    val queue:Queue<Int> = LinkedList<Int>()
 
-    var pathLength = 1
+    queue.add(x)
+    val index = x
+    var n = 0
 
-    while(queue.isNotEmpty()){
-        val size = queue.size
+    visited[index] = 1
 
-        for(i in 0 until size){
-            val current = queue.poll()
+    while(!queue.isEmpty()){
+        n = queue.remove()
 
-            if(current == end){
-                return pathLength
-            }
-
-            for(j in 0 until 4){
-                val newRow = current.row + dRow[j]
-                val newCol = current.col + dCol[j]
-
-                if(isValidMove(newRow, newCol) && !visited[newRow][newCol]){
-                    queue.offer(Point(newRow, newCol))
-                    visited[newRow][newCol] = true
-                }
-            }
+        if(n == k){
+            return visited[n]-1
         }
-        pathLength++
+        if(n-1 >= 0 && visited[n-1] == 0){
+            visited[n-1] = visited[n]+1
+            queue.add(n-1)
+        }
+        if(n+1 <= 100000 && visited[n+1] == 0){
+            visited[n+1] = visited[n]+1
+            queue.add(n+1)
+        }
+        if(2*n <= 100000 && visited[n*2] == 0){
+            visited[n*2] = visited[n]+1
+            queue.add(n*2)
+        }
     }
     return -1
-}
-
-fun isValidMove(row: Int, col: Int): Boolean{
-    val rowSize = maze.size
-    val colSize = maze[0].size
-    return row in 0 until rowSize && col in 0 until colSize && maze[row][col] != 0
 }
