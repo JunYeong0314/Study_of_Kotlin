0c8e1c6b3e6d6ab7d20bd16750e4ca75e75d8ba0
JunYeong0314 <kkmtk00314@naver.com>
2023-07-11 16:50:43 +0900

    baekjoon 16928

--- Baekjoon_prepare/src/main/kotlin/Main.kt
+++ Baekjoon_prepare/src/main/kotlin/Main.kt
@@ -1,75 +1,54 @@
+import java.util.ArrayDeque
 import java.util.Arrays
 import java.util.LinkedList
 import java.util.Queue
+import java.util.Stack
 import kotlin.concurrent.fixedRateTimer
 
-val dz = arrayOf(1, -1, 0, 0, 0 ,0)
-val dx = arrayOf(0, 0, 1, -1, 0, 0)
-val dy = arrayOf(0 ,0, 0, 0, 1, -1)
-
-lateinit var tomato: Array<Array<Array<Int>>>
-val isArr = mutableListOf<Triple<Int, Int, Int>>()
-
+val board = Array(101){ 0 }
+val visited = Array(101){ false }
 fun main() {
-    val (y, x, z) = readLine()?.split(" ")?.map { it.toInt() } ?: return
-    tomato = Array(z){Array(x){ Array(y){ 0 }}}
+    val (n, m) = readLine()?.split(" ")?.map { it.toInt() } ?: return
 
-
-    for(i in 0 until z){
-        for(j in 0 until x){
-            val element = readLine()?.split(" ")?.map { it.toInt() } ?: return
-            if(element.size != y) println("element is not correct!")
-            for(k in 0 until y){
-                tomato[i][j][k] = element[k]
-                if(element[k] == 1) isArr.add(Triple(i, j, k))
-            }
-        }
+    for (i in 0 until n) {
+        val (x, y) = readLine()?.split(" ")?.map { it.toInt() } ?: return
+        board[x] = y
     }
-    val result = bfs(x, y, z)
-    print(result)
+    for (i in 0 until m) {
+        val (u, v) = readLine()?.split(" ")?.map { it.toInt() } ?: return
+        board[u] = v
+    }
+    diceCnt(1, 0)
 }
 
-fun bfs(x: Int, y: Int, z: Int): Int{
-    val queue: Queue<Triple<Int, Int, Int>> = LinkedList<Triple<Int, Int, Int>>()
-    var cnt = -1
-
-    for(i in 0 until isArr.size){
-        queue.offer(isArr[i])
-    }
+fun diceCnt(start: Int, cnt: Int){
+    val queue: Queue<Pair<Int, Int>> = LinkedList<Pair<Int, Int>>()
+    queue.offer(Pair(start, cnt))
 
     while(queue.isNotEmpty()){
-        val qSize = queue.size
+        val cur = queue.peek().first
+        val curCnt = queue.peek().second
+        queue.poll()
 
-        for(i in 0 until qSize){
-            val cur = queue.poll()
-
-            for(j in 0 until 6){
-                val curZ = cur.first + dz[j]
-                val curX = cur.second + dx[j]
-                val curY = cur.third + dy[j]
-
-                if(isValidIndex(curZ, curX, curY, z, x, y) && tomato[curZ][curX][curY] != -1 && tomato[curZ][curX][curY] != 1){
-                    tomato[curZ][curX][curY] = 1
-                    queue.offer(Triple(curZ, curX, curY))
-                }
+        for(i in 1 until 7){
+            var next = cur + i
+            if(next == 100){
+                print(curCnt+1)
+                return
             }
-        }
-        cnt++
-    }
-
-    tomato.forEach { num->
-        num.forEach { row->
-            row.forEach { col->
-                if(col == 0){
-                    cnt = -1
+            else if(next < 100){
+                if(board[next] != 0){
+                    next = board[next]
+                }
+                if(!visited[next]){
+                    queue.offer(Pair(next, curCnt+1))
+                    visited[next] = true
                 }
             }
         }
     }
 
-    return cnt
 }
 
-fun isValidIndex(z: Int, x: Int, y: Int, nz: Int, nx: Int, ny: Int): Boolean{
-    return z in 0 until nz && x in 0 until nx && y in 0 until ny
-}
+
+
