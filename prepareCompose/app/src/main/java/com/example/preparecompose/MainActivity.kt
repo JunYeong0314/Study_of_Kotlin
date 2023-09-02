7f9cd0f8ae08df1ad73f51dc8345bd5cf8a10252
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2023-09-02 13:33:03 +0900

    Compose View

--- prepareCompose/app/src/main/java/com/example/preparecompose/MainActivity.kt
+++ prepareCompose/app/src/main/java/com/example/preparecompose/MainActivity.kt
@@ -1,114 +1,100 @@
 package com.example.preparecompose
-
-import android.graphics.Color
 import android.os.Bundle
 import android.provider.CalendarContract.Colors
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialogDefaults.shape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScaffoldDefaults
-import androidx.compose.material3.SnackbarData
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.preparecompose.ui.theme.PrepareComposeTheme
-import kotlinx.coroutines.CoroutineScope
+import com.example.preparecompose.ui.theme.Purple80
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserCard {
-
+            MyApp{
+                MyScreenContent()
             }
-
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-private fun UserData(){
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = painterResource(id = R.drawable.egologo),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .padding(8.dp, 12.dp, 8.dp, 12.dp)
-                .size(44.dp)
-                .clip(shape = CircleShape)
+fun DefaultPreview(){
+    MyApp {
+        MyScreenContent()
+    }
+}
+
+@Composable
+fun Greeting(name: String){
+
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+}
+
+
+@Composable
+fun MyApp(content: @Composable () -> Unit){
+    PrepareComposeTheme() {
+        Surface(color = Purple80) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = List(100){"안드로이드 #$it"}){
+    val counterState = remember{ mutableStateOf(0) }
+
+    Column(modifier = Modifier.fillMaxHeight()){
+        NameList(names = names, Modifier.weight(1f))
+        Counter(
+            count = counterState.value,
+            updateCount = {newCount ->
+                counterState.value = newCount
+            }
         )
-        Column {
-            Text("name")
-            Text(
-                "lastSeenOnline",
-                Modifier.padding(0.dp, 0.dp, 8.dp, 0.dp)
-            )
-            
-        }
     }
 }
-
 
 @Composable
-private fun UserCard(
-    onClick: () -> Unit
-){
-    val padding = 16.dp
-
-    Column(
-        Modifier
-            .clickable(onClick = onClick)
-            .padding(padding)
-            .fillMaxWidth()
-    ) {
-        UserData()
-        Spacer(Modifier.size(padding))
-
-        Card(){
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = null,
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.Crop
-            )
-        }
-
+fun Counter(count: Int, updateCount: (Int) -> Unit){
+    Button(
+        onClick = {updateCount(count+1)},
+        colors = ButtonDefaults.buttonColors(
+            if(count > 5) Color.Black else Color.Blue
+        )
+    ){
+        Text("$count 번 클릭하셨네요!")
     }
-
 }
 
-
+@Composable
+fun NameList(names: List<String>, modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier){
+        items(items = names){name->
+            Greeting(name = name)
+            Divider(color = Color.Black)
+        }
+    }
+}
 
