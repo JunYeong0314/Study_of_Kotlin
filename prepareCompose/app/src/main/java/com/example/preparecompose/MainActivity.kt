8eba9143b69ecb697d2712a028ad1815d21ffcf2
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2023-09-04 13:42:57 +0900

    Compose 2

--- prepareCompose/app/src/main/java/com/example/preparecompose/MainActivity.kt
+++ prepareCompose/app/src/main/java/com/example/preparecompose/MainActivity.kt
@@ -1,100 +1,139 @@
 package com.example.preparecompose
+import android.content.res.Configuration
 import android.os.Bundle
 import android.provider.CalendarContract.Colors
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.preparecompose.ui.theme.PrepareComposeTheme
 import com.example.preparecompose.ui.theme.Purple80
+import androidx.compose.runtime.getValue import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp{
-                MyScreenContent()
+            PrepareComposeTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(SampleData.conversationSample)
+                    
+                }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+data class Message(val author: String, val body: String)
+
+/*@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mdoe"
+)
 @Composable
-fun DefaultPreview(){
-    MyApp {
-        MyScreenContent()
-    }
-}
-
-@Composable
-fun Greeting(name: String){
-
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-}
-
-
-@Composable
-fun MyApp(content: @Composable () -> Unit){
-    PrepareComposeTheme() {
-        Surface(color = Purple80) {
-            content()
+fun PrevireMessgeCard(){
+    PrepareComposeTheme {
+        Surface{
+            MessageCard(Message("Ban", "Hello, I'm Ban"))
         }
     }
-}
-
+}*/
 @Composable
-fun MyScreenContent(names: List<String> = List(100){"안드로이드 #$it"}){
-    val counterState = remember{ mutableStateOf(0) }
+fun MessageCard(msg: Message){
+    Row(modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(id = R.drawable.egologo),
+            contentDescription = "logo",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
 
-    Column(modifier = Modifier.fillMaxHeight()){
-        NameList(names = names, Modifier.weight(1f))
-        Counter(
-            count = counterState.value,
-            updateCount = {newCount ->
-                counterState.value = newCount
+        var isExpanded by remember{ mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
             }
-        )
-    }
-}
-
-@Composable
-fun Counter(count: Int, updateCount: (Int) -> Unit){
-    Button(
-        onClick = {updateCount(count+1)},
-        colors = ButtonDefaults.buttonColors(
-            if(count > 5) Color.Black else Color.Blue
-        )
-    ){
-        Text("$count 번 클릭하셨네요!")
-    }
-}
-
-@Composable
-fun NameList(names: List<String>, modifier: Modifier = Modifier){
-    LazyColumn(modifier = modifier){
-        items(items = names){name->
-            Greeting(name = name)
-            Divider(color = Color.Black)
         }
     }
 }
 
+@Composable
+fun Conversation(message: List<Message>){
+    LazyColumn{
+        items(message){message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun PreviewConversation(){
+    PrepareComposeTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
