d4aadd30da400b38be2f65b573a70d7f06edcc8e
JunYeong Kim <96372487+JunYeong0314@users.noreply.github.com>
2023-09-07 16:40:35 +0900

    prepare

--- prepareCompose/app/src/main/java/com/example/preparecompose/MainActivity.kt
+++ prepareCompose/app/src/main/java/com/example/preparecompose/MainActivity.kt
@@ -3,59 +3,47 @@ import android.content.res.Configuration
 import android.os.Bundle
 import android.provider.CalendarContract.Colors
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Divider
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.currentRecomposeScope
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.preparecompose.ui.theme.PrepareComposeTheme
-import com.example.preparecompose.ui.theme.Purple80
-import androidx.compose.runtime.getValue import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PrepareComposeTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    Conversation(SampleData.conversationSample)
-                    
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
@@ -77,63 +65,69 @@ fun PrevireMessgeCard(){
         }
     }
 }*/
-@Composable
-fun MessageCard(msg: Message){
-    Row(modifier = Modifier.padding(all = 8.dp)){
-        Image(
-            painter = painterResource(id = R.drawable.egologo),
-            contentDescription = "logo",
-            modifier = Modifier
-                .size(40.dp)
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-
-        var isExpanded by remember{ mutableStateOf(false) }
-        val surfaceColor by animateColorAsState(
-            if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
-        )
-
-        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
-            Text(
-                text = msg.author,
-                color = MaterialTheme.colorScheme.secondary,
-                style = MaterialTheme.typography.titleMedium
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Surface(
-                shape = MaterialTheme.shapes.medium,
-                shadowElevation = 1.dp,
-                color = surfaceColor,
-                modifier = Modifier.animateContentSize().padding(1.dp)
-            ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-        }
-    }
-}
 
 @Composable
-fun Conversation(message: List<Message>){
-    LazyColumn{
-        items(message){message ->
-            MessageCard(message)
-        }
-    }
-}
-
-@Preview(
-    showBackground = true
-)
-@Composable
-fun PreviewConversation(){
+fun MyApp(content: @Composable (PaddingValues) -> Unit){
+    val context = LocalContext.current
+    val state = rememberScaffoldState()
     PrepareComposeTheme {
-        Conversation(SampleData.conversationSample)
+        Scaffold(
+            scaffoldState = state,
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Title text") },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                            },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.Default.Menu,
+                                    contentDescription = "Menu"
+                                )
+                            }
+                        )
+                    },
+                    actions = {
+                        Icon(
+                            imageVector = Icons.Default.Favorite,
+                            contentDescription = "Favorite"
+                        )
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+                )
+            },
+            drawerContent = {
+                Icon(
+                    modifier = Modifier.padding(14.dp),
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Person"
+                )
+                Text(modifier = Modifier.padding(14.dp), text = "Jun")
+                Text(modifier = Modifier.padding(14.dp), text = "Kam")
+                Text(modifier = Modifier.padding(14.dp), text = "NKS")
+            },
+        ) {
+            content(it)
+        }
+    }
+}
+
+@Composable
+fun MainContent(){
+    Surface(color = MaterialTheme.colors.background) {
+        Text(text = "Hello")
+        
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview(){
+    MyApp {it->
+        Text(text = "Hello")
     }
 }
